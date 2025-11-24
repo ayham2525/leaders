f33508eecb194c0808e7e28ff6931f1dcc257b28
Ayham Al Khoury <ayham252@gmail.com>
2025-11-24 17:20:48 +0000

    sss

---
+++ site/languages/wpml/queue/wp-migrate-db_pending.php
@@ -0,0 +1,2 @@
+<?php
+return ['items'=>['Invalid content verification signature, please verify the connection information on the remote site and try again.'=>['saveStringInDb'=>true,'cmp'=>['WordPress',3],'names'=>[],'urls'=>[]]]];
