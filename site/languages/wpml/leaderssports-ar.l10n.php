b5ecce41d2b740dd3ccbabbdbdadfca2a058f980
Ayham Al Khoury <ayham252@gmail.com>
2025-12-16 11:12:57 +0000

    sss

---
+++ site/languages/wpml/leaderssports-ar.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['messages'=>['Days:'=>'الأيام:','Fees:'=>'الرسوم:','Post Type General NameSports Activities'=>'الفعاليات الرياضية','Sports Activities'=>'الفعاليات الرياضية','View Sport'=>'عرض الرياضة','View Sports'=>'عرض الرياضات']];
---
+++ site/languages/wpml/queue/main-theme_pending.php
@@ -0,0 +1,2 @@
+<?php
+return ['items'=>['No FAQs added yet.'=>['saveStringInDb'=>true,'cmp'=>['wp_bootstrap_starter',2],'names'=>[],'urls'=>[]]]];
