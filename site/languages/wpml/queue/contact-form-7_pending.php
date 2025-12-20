0e7ae2e7385097e4567a83006d408e71187ad818
Ayham Al Khoury <ayham252@gmail.com>
2025-12-20 06:00:15 +0000

    sss

--- site/languages/wpml/queue/contact-form-7_pending.php
+++
@@ -1,2 +0,0 @@
-<?php
-return ['items'=>['Contact Form 7'=>['saveStringInDb'=>true,'cmp'=>['WordPress',3],'names'=>[],'urls'=>[]]]];
--- site/languages/wpml/queue/main-theme_pending.php
+++
@@ -1,2 +0,0 @@
-<?php
-return ['items'=>['No FAQs added yet.'=>['saveStringInDb'=>true,'cmp'=>['wp_bootstrap_starter',2],'names'=>[],'urls'=>[]]]];
